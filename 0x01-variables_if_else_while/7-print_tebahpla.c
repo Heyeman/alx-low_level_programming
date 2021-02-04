a35f2d40882cd07abb09fa2e7ceaeda163233b3b
Heyeman <abdisaheyeman083@gmail.com>
2021-02-04 11:19:30 +0000

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x = 'z';
+while (x >= 'a')
+{
+putchar(x);
+x--;
+}
+putchar('\n');
+return (0);
+}
