6504a64f24c485de8dc2ef093b90a749b2cff54e
Heyeman <59885488+Heyeman@users.noreply.github.com>
2021-02-04 14:55:16 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a = 0;
+char b = 0;
+while (a < 10)
+{
+while (b < 10)
+{
+putchar(a + '0');
+putchar(b + '0');
+b++;
+}
+a++;
+}
+return (0);
+}
