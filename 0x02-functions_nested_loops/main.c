ee4990d33d6ee2025e9774bf2b23feac986ce2e6
Heyeman <abdisaheyeman083@gmail.com>
2021-02-09 10:23:39 +0000

    new commit

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
-#include "1-alphabet.c"
 #include "_putchar.c"
+#include "holberton.h"
+#include "2-print_alphabet_x10.c"
 
 int main(void)
 {
-print_alphabet();
+print_alphabet_x10();
 return (0);
 }
