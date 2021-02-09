a3a4538fe6b56e5af11859eb13f6cdfd93722d5b
Heyeman <59885488+Heyeman@users.noreply.github.com>
2021-02-09 17:03:29 +0300

    Update holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,9 +1,11 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif
+#endif /* HOLBERTON_H */
