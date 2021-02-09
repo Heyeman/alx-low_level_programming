b18285d997ff0e3bf980f05004aa53328a6f2e4c
Heyeman <abdisaheyeman083@gmail.com>
2021-02-09 11:01:38 +0000

    new commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _abs - func
+ * @c: param
+ * Return: 0
+ */
+int _abs(int c)
+{
+if (c < 0)
+c = -c;
+return (c);
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,10 +1,11 @@
 #include "_putchar.c"
+#include <stdio.h>
 #include "holberton.h"
-#include "5-sign.c"
+#include "6-abs.c"
 
 int main(void)
 {
-_putchar('H');
- _putchar(print_sign(20020) + '0');
+  _putchar('h');
+  printf("%d",_abs(-200));
 return (0);
 }
