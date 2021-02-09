c5be8001588a5236dc9581452b4011351c2a1046
Heyeman <abdisaheyeman083@gmail.com>
2021-02-09 10:14:53 +0000

    new commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet - function
+ *
+ *
+ * Return - success
+ */
+void print_alphabet(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+#include "1-alphabet.c"
+#include "_putchar.c"
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
