2869e8352b899418c4ef172e94ad34d002e3673c
Heyeman <abdisaheyeman083@gmail.com>
2021-02-09 11:13:06 +0000

    new commit

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,11 +1,16 @@
 #include "_putchar.c"
 #include <stdio.h>
 #include "holberton.h"
-#include "6-abs.c"
+#include "7-print_last_digit.c"
 
 int main(void)
 {
-  _putchar('h');
-  printf("%d",_abs(-200));
+  int r;
+
+  print_last_digit(98);
+  print_last_digit(0);
+  r = print_last_digit(-1024);
+  _putchar('0' + r);
+  _putchar('\n');
 return (0);
 }
