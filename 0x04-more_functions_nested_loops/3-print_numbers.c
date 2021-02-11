39ae0af5b6de24a120ae799b5f462bb0cd37cac0
Heyeman <59885488+Heyeman@users.noreply.github.com>
2021-02-11 10:36:02 -0800

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ *
+ * Return: No.
+ */
+
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
