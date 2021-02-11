28fc679b43cceedfc4c209f1daf90b05a9900ff7
Heyeman <59885488+Heyeman@users.noreply.github.com>
2021-02-11 10:39:00 -0800

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints n
+ * @n: size
+ * Return: none.
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
