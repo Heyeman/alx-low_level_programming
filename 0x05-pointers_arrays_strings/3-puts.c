44044ea1b4ab71164712f1548940f1f7a072cc04
Heyeman <abdisaheyeman083gmail.com>
2021-02-16 09:07:26 +0000

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,81 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+  while (*str != '\0')
+    {
+      _putchar(*str);
+      str++;
+    }
+  _putchar('\n');
+}#include "holberton.h"
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+  while (*str != '\0')
+    {
+      _putchar(*str);
+      str++;
+    }
+  _putchar('\n');
+}#include "holberton.h"
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+  while (*str != '\0')
+    {
+      _putchar(*str);
+      str++;
+    }
+  _putchar('\n');
+}#include "holberton.h"
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+  while (*str != '\0')
+    {
+      _putchar(*str);
+      str++;
+    }
+  _putchar('\n');
+}#include "holberton.h"
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+  while (*str != '\0')
+    {
+      _putchar(*str);
+      str++;
+    }
+  _putchar('\n');
+}
