dbd084537ea8dc254d6a25d5ff19c2c22ae25dd5
Heyeman <59885488+Heyeman@users.noreply.github.com>
2021-03-18 12:08:44 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,23 +1,19 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_list - frees a list_t list
- * @head: pointer to the start of the list
- *
- * Return: void
+ * free_list - frees a linked list
+ * @head: a pointer to the first struct in the linked list
+ * Return: No Value
  */
 void free_list(list_t *head)
 {
-	list_t *current, *next;
+	list_t *temp;
 
-	current = head;
-	while (current != NULL)
+	for (; head;)
 	{
-		next = current->next;
-		free(current->str);
-		free(current);
-		current = next;
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
 	}
-
 }
